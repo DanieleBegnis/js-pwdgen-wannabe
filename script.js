f5077b6e7153371c370f338625cf87f13ae9650d
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-04 16:32:05 +0100

    feat: add password h3

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+//Aggiungi nome password
 const userName =  prompt('Scrivi il tuo nome')
-
+//aggiungi cognome password
 const userSurname =  prompt('Scrivi il tuo cognome')
-
+//aggiungi colore preferito password
 const favouriteColor = prompt('Scrivi il tuo colore preferito')
+//somma delle tre variabili
+const userPassword = `La tua nuova password è ${userName}${userSurname}${favouriteColor}23`;
+//mostra password
+document.getElementById('message').innerHTML = userPassword;
