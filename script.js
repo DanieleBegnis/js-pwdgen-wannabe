2632730a8d96c3c82fd33fd29487d96a0381f6a9
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-04 16:25:47 +0100

    fix: add apostrofo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const userName =  prompt(Scrivi il tuo nome)
+const userName =  prompt('Scrivi il tuo nome')
 
-const userSurname =  prompt(Scrivi il tuo cognome)
+const userSurname =  prompt('Scrivi il tuo cognome')
+
+const favouriteColor = prompt('Scrivi il tuo colore preferito')
